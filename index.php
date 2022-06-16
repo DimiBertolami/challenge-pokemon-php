1a6be5797fa296d6dc6422177aeaa7034c05352b
Dimitri Bertolami <dimibertolami@gmail.com>
2022-06-16 12:02:56 +0200

    moves abilities

--- index.php
+++ index.php
@@ -1,91 +1,89 @@
 <?php
-include('url.php');
-include('getInfo.php');
-?>
 
-<!DOCTYPE html>
-<html>
+
+$pokeName = $_POST['pokemonName'];
+//echo $pokeName;
+$pokeURL = "https://pokeapi.co/api/v2/pokemon/$pokeName";
+//echo $pokeURL;
+//$data = file_get_contents($pokeURL);
+$data = file_get_contents($pokeURL);
+$decoded_Data = json_decode($data, true);
+//echo "<br>";
+//echo "$ID $Name";
+//echo $Species;
+//echo $decoded_Data[0];
+//var_dump($decoded_Data);
+$Name = $decoded_Data['name'];
+$ID = $decoded_Data['id'];
+//echo $Name;
+//echo "<br>";
+//echo $ID;
+//echo "<br>";
+$image = $decoded_Data['sprites']['front_default'];
+//echo $image;
+//$abilities = $decoded_Data['abilities[0].ability.name'];
+//array_walk($abilities, '');
+//var_dump($abilities);
+$abilityName =  $decoded_Data['abilities'];
+//print_r($abilityName);
+for ($i = 0; $i < count($abilityName); $i++) {
+    echo $abilityName[$i]['ability']['name'];
+    echo '<br>';
+}
+$moves = $decoded_Data['moves'];
+for ($i = 0; $i < count($moves); $i++) {
+    echo $moves[$i]['move']['name'];
+    echo '<br>';
+}
+
+
+
+
+?>
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Pokédex</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
-    <link rel="icon" type="image/png" href="fav-ic/pokeball.png"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>AJAX-Pokedex</title>
 </head>
-<body>
-<h1>Pokedex</h1>
-<form class="formSearch" action="" method="get">
-    <input class="input" type="text" name="pokemon" placeholder="Search any Pokemon">
-    <input class="submitfirst" value="Search" type="submit">
-</form>
-<!--
-<form class="formDisplay"action="" method="get">
-    <input class ="input" type="number" name="displaypokemon" placeholder="How many pokemon you wanna display">
-    <input class ="submitfirst" value="Display" type="submit">
-</form>
--->
-<div class="list-pokemon">
+<body border="2em solid black">
 
-    <!-- Pokémon display 151 pokémon ----------------------------------------------- -->
-    <?php foreach ($pokemon as $_pokemon): ?>
-        <div class="list-item-pokemon">
-            <p class="pokeid"><?= pokemonId($_pokemon->name, 1) ?>.</p>
-            <div class="sprites-container">
-                <img class="sprite" src="<?= pokemonSprite($_pokemon->name, 2, 0) ?>">
-            </div>
-            <p class="pokename"><?= $_pokemon->name ?></p>
-            <p class="poketype">Type: <?= pokemonType($_pokemon->name, 3, 1) ?></p>
-        </div>
-
-        <!-- Pokémon Pannel on click ----------------------------------------------- -->
-        <div class="pokeinfo">
-            <h2>Pokémon Information</h2>
-            <p class="pokeinfoname"><?= $_pokemon->name ?></p>
-            <p class="pokeinfotype"><strong>Type : </strong><?= pokemonType($_pokemon->name, 3, 1) ?><br/></p>
-            <p class="pokeinfoabilities"><strong>Abilities : </strong><?= pokemonAbilities($_pokemon->name, 4, 1) ?></p>
-            <img class="infosprite" src="<?= pokemonSprite($_pokemon->name, 2, 0) ?>">
-            <img class="infosprite2" src="<?= pokemonSpriteShiny($_pokemon->name, 3, 0) ?>">
-            <p class="close-button">Close information</p>
-            <p class="pokeinfodescription"><strong>Description
-                    : </strong><?= pokemonDescription($_pokemon->name, 4, 1) ?></p>
-            <p class="pokeheight"><strong>Height : </strong><?= pokemonHeight($_pokemon->name, 4, 1) ?>0cm <br/></p>
-            <p class="pokeweight"><strong>Weight : </strong><?= pokemonWeight($_pokemon->name, 4, 1) ?>Kg</p>
-            <p class="shinyform">Shiny <?= $_pokemon->name ?></p>
-        </div>
-    <?php endforeach; ?>
-
-    <!-- Pokémon Pannel Search ----------------------------------------------- -->
-    <?php if (!empty($_GET['pokemon'])): ?>
-        <div class="pokeinfo pokesearch" style="display : flex">
-            <h2>Pokémon Information</h2>
-            <p class="pokeinfoname"><?= $searchedPokemon ?></p>
-            <p class="pokeinfotype"><strong>Type : </strong><?= pokemonType($searchedPokemon, 3, 1) ?><br/></p>
-            <p class="pokeinfoabilities"><strong>Abilities : </strong><?= pokemonAbilities($searchedPokemon, 4, 1) ?>
-            </p>
-            <img class="infosprite" src="<?= pokemonSprite($searchedPokemon, 2, 0) ?>">
-            <img class="infosprite2" src="<?= pokemonSpriteShiny($searchedPokemon, 3, 0) ?>">
-            <p class="close-button2">Close information</p>
-            <p class="pokeinfodescription"><strong>Description
-                    : </strong><?= pokemonDescription($searchedPokemon, 4, 1) ?></p>
-            <p class="pokeheight"><strong>Height : </strong><?= pokemonHeight($searchedPokemon, 4, 1) ?>0cm <br/></p>
-            <p class="pokeweight"><strong>Weight : </strong><?= pokemonWeight($searchedPokemon, 4, 1) ?>Kg</p>
-            <p class="shinyform">Shiny <?= $searchedPokemon ?></p>
-
-            <!-- Close the $searchedPokemon Pannel ----------------------------------------------- -->
-            <script>
-                const closeButton2 = document.querySelector('.close-button2')
-                const infopannel2 = document.querySelector(".pokesearch")
-
-                closeButton2.addEventListener(
-                    'click', () => {
-                        infopannel2.style.display = 'none'
-                    })
-            </script>
-
-        </div>
-    <?php endif; ?>
+<img src='<?php echo $image; ?>' alt="1" title="<?php echo $Name; ?>">
+<span id="target">
+</span>
+<div class="poke_box">
 </div>
-<script src="ajax-pokedex.js"></script>
+<div class="child-to-body box">
+    <form method="post">
+        <input type="text" id="pokemonName" value="charmander" class="button" name="pokemonName">
+        <input type="submit" class="button" name="Go" id="Go" value="Go">
+    </form>
+    <label for="id" style="top: 50px; left:50px"></label><div id="id" style="top: 50px; left:20px"></div>
+    <label for="abilities" style="top: 65px; left:40px"></label><div id="abilities" style="top: 80px; left:50px"></div>
+    <label for="moves" style="top: 160px; left:20px"></label><div id="moves" style="top: 160px; left:50px"></div>
+    <div class="pokeball">
+        <div class="pokeball__button" id="pokeball__button">
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+<!--<script src="pokedex.js"></script>-->
+
 </body>
 </html>
+
+
+
