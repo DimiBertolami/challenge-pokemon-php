a09bca6132fdf183429b9fea954e801175b6bff2
Dimitri Bertolami <dimibertolami@gmail.com>
2022-06-17 15:32:00 +0200

    evolutions are working!

--- index.php
+++ index.php
@@ -13,21 +13,76 @@ $data = file_get_contents($pokeURL);
 $decoded_Data = json_decode($data, true);
 $Name = $decoded_Data['name'];
 $ID = $decoded_Data['id'];
-$Name= "$Name($ID)";
-// echo $Name;
-$image = $decoded_Data['sprites']['front_default'];
+$speciesUrl = $decoded_Data['species']['url'];
+//print_r($speciesUrl);
+$evolutionData = file_get_contents($speciesUrl);
+$decoded_EvoData = json_decode($evolutionData, true);
+$evolutionUrl = $decoded_EvoData['evolution_chain']['url'];
+//print_r($evolutionUrl);
+$evolutions = file_get_contents($evolutionUrl);
+$decoded_Evolutions = json_decode($evolutions, true);
+//var_dump($decoded_Evolutions);
+//$decoded_Evolutions->chain->evolves_to[0]->evolves_to[0]->species->url
+//$decoded_Evolutions->chain->evolves_to[0]->species->url
+$evolvesToUrl = $decoded_Evolutions['chain']['evolves_to'][0]['species']['url'];
+$evolvesToName = $decoded_Evolutions['chain']['evolves_to'][0]['species']['name'];
+$evolvesToTootUrl = $decoded_Evolutions['chain']['evolves_to'][0]['evolves_to'][0]['species']['url'];
+$evolvesToTootName = $decoded_Evolutions['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'];
+$pokeURL2 = "https://pokeapi.co/api/v2/pokemon/$evolvesToName";
+$pokeURL3 = "https://pokeapi.co/api/v2/pokemon/$evolvesToTootName";
+//print_r($evolvesToUrl);
+$data2 = file_get_contents($pokeURL2);
+$decoded_Data2 = json_decode($data2, true);
+$Name2 = $decoded_Data2['name'];
+$ID2 = $decoded_Data2['id'];
 
-$abilityName =  $decoded_Data['abilities'];
+$data3 = file_get_contents($pokeURL3);
+$decoded_Data3 = json_decode($data3, true);
+$Name3 = $decoded_Data3['name'];
+$ID3 = $decoded_Data3['id'];
+$Name= "$Name($ID)";
+//$Name2= "$Name2($ID2)";
+//$Name3= "$Name3($ID3)";
+// echo $Name;
+$image  =  $decoded_Data['sprites']['front_default'];
+$image2 = $decoded_Data2['sprites']['front_default'];
+$image3 = $decoded_Data3['sprites']['front_default'];
+
+$abilityName  =   $decoded_Data['abilities'];
+$abilityName2 =  $decoded_Data2['abilities'];
+$abilityName3 =  $decoded_Data3['abilities'];
 for ($i = 0; $i < count($abilityName); $i++) {
-    $Name .= $abilityName[$i]['ability']['name'] . "(=ability), ";
-// //    echo ', ';
-// //    echo '<br>';
+    if ($i < 40) {
+        $Name .= $abilityName[$i]['ability']['name'] . "(=ability), ";
+    }
+}
+for ($i = 0; $i < count($abilityName2); $i++) {
+    if($i<40){
+        $Name2 .= $abilityName2[$i]['ability']['name'] . "(=ability), ";
+    }
+}
+for ($i = 0; $i < count($abilityName3); $i++) {
+    if($i<40){
+        $Name3 .= $abilityName3[$i]['ability']['name'] . "(=ability), ";
+    }
 }
 $moves = $decoded_Data['moves'];
+$moves2 = $decoded_Data2['moves'];
+$moves3 = $decoded_Data3['moves'];
 for ($i = 0; $i < count($moves); $i++) {
-    $Name .= $moves[$i]['move']['name'] . "(=move), ";
-// //    echo ', ';
-// //    echo '<br>';
+    if($i<40){
+        $Name .= $moves[$i]['move']['name'] . "(=move), ";
+    }
+}
+for ($i = 0; $i < count($moves2); $i++) {
+    if($i<40) {
+        $Name2 .= $moves2[$i]['move']['name'] . "(=move), ";
+    }
+}
+for ($i = 0; $i < count($moves3); $i++) {
+    if($i<40) {
+    $Name3 .= $moves3[$i]['move']['name'] . "(=move), ";
+    }
 }
 ?>
 <!doctype html>
@@ -42,10 +97,10 @@ for ($i = 0; $i < count($moves); $i++) {
     <title>AJAX-Pokedex</title>
 </head>
 <body border="2em solid black">
-<img src='<?php echo $image; ?>' alt="1" title="<?php echo $Name; ?>">
-<h6><?php print_r($Name); ?></h6>
 <span id="target">
-
+    <img src='<?php echo $image; ?>' alt="1" title="<?php echo $Name; ?>">
+    <img src='<?php echo $image2; ?>' alt="2" title="<?php echo $Name2; ?>">
+    <img src='<?php echo $image3; ?>' alt="3" title="<?php echo $Name3; ?>">
 </span>
 <div class="poke_box">
 </div>
